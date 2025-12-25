9bf6bf3a5ee339db9b2898f95b9cfd9541d7d61b
edenavital <edendor@gmail.com>
2025-12-25 11:49:51 +0200

    Update iac.tf

--- iac.tf
+++ iac.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket" "data" {
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
     }, {
-    git_commit           = "4d57f83ca4d3a78a44fb36d1dcf0d23983fa44f5"
+    git_commit           = "4d57f83ca4d3a78a44fb36x1dcf0d23983fa44f5"
     git_file             = "terraform/aws/s3.tf"
     git_last_modified_at = "2022-05-18 07:08:06"
     git_last_modified_by = "nimrod@bridgecrew.io"
